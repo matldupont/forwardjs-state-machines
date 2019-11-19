8df57615d4438dc32679de54472a5df668c6bbd0
Mat Dupont <dupont.mat.l@gmail.com>
2019-11-19 00:14:16 -0500

    initial commit

---
+++ theme.js
@@ -0,0 +1,17 @@
+export default {
+  googleFont:
+    "https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope",
+  fonts: {
+    body: '"Annie Use Your Telescope", cursive'
+  },
+  colors: {
+    text: "#fff",
+    background: "#111"
+  },
+  styles: {
+    root: {
+      fontSize: ["1.5em", null, "4em"],
+      textAlign: "center"
+    }
+  }
+};
